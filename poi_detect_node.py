07412dec8fc85edf86fc80338aea4bee4347a28a
baobaozi <516191859@qq.com>
2025-12-17 11:37:57 +0800

    feat: update

--- poi_detect_node.py
+++ poi_detect_node.py
@@ -1,14 +1,23 @@
+#!/home/unitree/poi_detect/poi_detect/venv/bin/python
+
 import os
 import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
 import rospy
 from std_msgs.msg import String
 
-
+import numpy as np
 import poi_detect
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+
+def image_callback(msg):
+    np_arr = np.frombuffer(msg.data, np.unit8)
+
+
+
 
 if __name__ == "__main__":
     result = poi_detect.poi_detect("/home/unitree/poi_detect/poi_detect/src/poi_algorithm/testdata")
-    rospy.loginfo("Test result: %s" % str(result))
+    print("Test result: %s" % str(result))
